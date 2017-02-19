db062e1f432e7829395ed28d37ccdf484dd1c28f
votuankiet <votuankiet162@gmail.com>
2017-02-19 18:23:14 +0800

    rdbms-backed resource City

--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "ApiDevelopments", type: :request do
+  def parsed_body
+    JSON.parse(response.body)
+  end
+
   describe "RDBMS-backed" do
-    it "create RDBMS-backed model"
-    it "expose RDBMS-backed API resource"
+    before(:each) { City.delete_all }
+    after(:each)  { City.delete_all }
+
+    it "create RDBMS-backed model" do
+      object=City.create(:name=>"test")
+      expect(City.find(object.id).name).to eq("test")
+    end
+
+    it "expose RDBMS-backed API resource" do
+      object=City.create(:name=>"test")
+      expect(cities_path).to eq("/api/cities")
+      get city_path(object.id)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["name"]).to eq("test")
+    end
   end
 
   describe "MongoDB-backed" do
