cf8a30175132ffd65e02b0f4a29fccf05ad81b82
votuankiet <votuankiet162@gmail.com>
2017-02-19 21:39:26 +0800

    add mongo-backed resources State

--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -24,7 +24,22 @@ RSpec.describe "ApiDevelopments", type: :request do
   end
 
   describe "MongoDB-backed" do
-    it "create MongoDB-backed model"
-    it "expose MongoDB-backed API resource"
+    before(:each) { State.delete_all }
+    after(:each)  { State.delete_all }
+
+    it "create MongoDB-backed model" do
+      object=State.create(:name=>"test")
+      expect(State.find(object.id).name).to eq("test")
+    end
+    
+    it "expose MongoDB-backed API resource" do
+      object=State.create(:name=>"test")
+      expect(states_path).to eq("/api/states")
+      get state_path(object.id) 
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["name"]).to eq("test")
+      expect(parsed_body).to include("created_at")
+      expect(parsed_body).to include("id"=>object.id.to_s)
+    end
   end
 end
