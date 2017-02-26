c0d110e3fcdc4fd5cd9d488d6e3492ba84442756
votuankiet <votuankiet162@gmail.com>
2017-02-26 14:14:32 +0800

    restrict not creating destroying route
    http://edgeguides.rubyonrails.org/routing.html#restricting-the-routes-created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope :api, defaults: {format: :json} do
-    resources :cities, except: [:new, :edit]
-    resources :states, except: [:new, :edit]
+    resources :cities, except: [:new, :edit, :destroy]
+    resources :states, except: [:new, :edit, :destroy]
   end
 
   get '/ui' => 'ui#index'
