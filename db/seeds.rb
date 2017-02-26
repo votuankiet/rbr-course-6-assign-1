f93adb2ac1d1a4cfb521208f468892892076503e
votuankiet <votuankiet162@gmail.com>
2017-02-26 13:59:31 +0800

    add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+City.delete_all
+State.delete_all
+City.create([{name: 'Baltimore'}])
+State.create([{name: 'Maryland'}])
